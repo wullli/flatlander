085de934c02b5daaab33018a8804a663291a8f95
Pascal <pascal.wullschleger@hslu.ch>
2020-08-06 22:57:54 +0200

    Prettify run script

--- run.py
+++ run.py
@@ -1,176 +1,51 @@
 import time
 
 import numpy as np
-from flatlander.env.rllib.flatland.observations.tree_obs import TreeObservation
-from flatlander.env.sb.flatland_gym_env import FlatlandGymEnv
-from stable_baselines import PPO2
-from stable_baselines.common.policies import MlpPolicy
-from stable_baselines.common.vec_env import DummyVecEnv
 
-from flatland.envs.rail_env import RailEnv
-from flatland.envs.rail_generators import sparse_rail_generator
-from flatland.envs.schedule_generators import sparse_schedule_generator
 from flatland.evaluators.client import FlatlandRemoteClient
 from flatlander.env.observations.default_observation_builder import CustomObservationBuilder
 
-#####################################################################
-# Instantiate a Remote Client
-#####################################################################
 remote_client = FlatlandRemoteClient()
 
-#####################################################################
-# Define your custom controller
-#
-# which can take an observation, and the number of agents and 
-# compute the necessary action for this step for all (or even some)
-# of the agents
-#####################################################################
 
-n_agents = 1
-x_dim = 25
-y_dim = 25
-n_cities = 4
-max_rails_between_cities = 2
-max_rails_in_city = 3
-seed = 42
-observation_tree_depth = 2
-
-observation = TreeObservation({'max_depth': observation_tree_depth, 'shortest_path_max_depth': 10})
-
-rail_env = RailEnv(
-    width=x_dim,
-    height=y_dim,
-    obs_builder_object=observation.builder(),
-    rail_generator=sparse_rail_generator(
-        max_num_cities=n_cities,
-        seed=seed,
-        grid_mode=False,
-        max_rails_between_cities=max_rails_between_cities,
-        max_rails_in_city=max_rails_in_city
-    ),
-    schedule_generator=sparse_schedule_generator(),
-    number_of_agents=n_agents,
-)
+def random_agent(_, n_agents: int):
+    _action = {}
+    for _idx in range(n_agents):
+        _action[_idx] = np.random.randint(0, 5)
+    return _action
 
 
-def get_env():
-    gym_env = FlatlandGymEnv(
-        rail_env=rail_env,
-        observation_space=observation.observation_space(),
-        regenerate_rail_on_reset=True,
-        regenerate_schedule_on_reset=True
-    )
-    return gym_env
+obs_builder = CustomObservationBuilder()
 
-
-env = DummyVecEnv([get_env])
-
-model = PPO2(MlpPolicy, env, verbose=1)
-model = model.load("./test_ppo.tar")
-
-#####################################################################
-# Instantiate your custom Observation Builder
-# 
-# You can build your own Observation Builder by following 
-# the example here : 
-# https://gitlab.aicrowd.com/flatland/flatland/blob/master/flatland/envs/observations.py#L14
-#####################################################################
-my_observation_builder = CustomObservationBuilder()
-
-# Or if you want to use your own approach to build the observation from the env_step, 
-# please feel free to pass a DummyObservationBuilder() object as mentioned below,
-# and that will just return a placeholder True for all observation, and you 
-# can build your own Observation for all the agents as your please.
-# my_observation_builder = DummyObservationBuilder()
-
-
-#####################################################################
-# Main evaluation loop
-#
-# This iterates over an arbitrary number of env evaluations
-#####################################################################
 evaluation_number = 0
 while True:
 
     evaluation_number += 1
-    # Switch to a new evaluation environemnt
-    # 
-    # a remote_client.env_create is similar to instantiating a 
-    # RailEnv and then doing a env.reset()
-    # hence it returns the first observation from the 
-    # env.reset()
-    # 
-    # You can also pass your custom observation_builder object
-    # to allow you to have as much control as you wish 
-    # over the observation of your choice.
     time_start = time.time()
     observation, info = remote_client.env_create(
-        obs_builder_object=my_observation_builder
+        obs_builder_object=obs_builder
     )
     env_creation_time = time.time() - time_start
     if not observation:
-        #
-        # If the remote_client returns False on a `env_create` call,
-        # then it basically means that your agent has already been 
-        # evaluated on all the required evaluation environments,
-        # and hence its safe to break out of the main evaluation loop
         break
 
     print("Evaluation Number : {}".format(evaluation_number))
 
-    #####################################################################
-    # Access to a local copy of the environment
-    # 
-    #####################################################################
-    # Note: You can access a local copy of the environment 
-    # by using : 
-    #       remote_client.env 
-    # 
-    # But please ensure to not make any changes (or perform any action) on 
-    # the local copy of the env, as then it will diverge from 
-    # the state of the remote copy of the env, and the observations and 
-    # rewards, etc will behave unexpectedly
-    # 
-    # You can however probe the local_env instance to get any information
-    # you need from the environment. It is a valid RailEnv instance.
     local_env = remote_client.env
     number_of_agents = len(local_env.agents)
 
-    # Now we enter into another infinite loop where we 
-    # compute the actions for all the individual steps in this episode
-    # until the episode is `done`
-    # 
-    # An episode is considered done when either all the agents have 
-    # reached their target destination
-    # or when the number of time steps has exceed max_time_steps, which 
-    # is defined by : 
-    #
-    # max_time_steps = int(4 * 2 * (env.width + env.height + 20))
-    #
     time_taken_by_controller = []
     time_taken_per_step = []
     steps = 0
     while True:
-        #####################################################################
-        # Evaluation of a single episode
-        #
-        #####################################################################
-        # Compute the action for this step by using the previously 
-        # defined controller
         time_start = time.time()
         action_list = []
-        for i in range(number_of_agents):
-            act = model.step(observation[i])
-            action_list.append(act)
+
+        action = random_agent(observation, n_agents=number_of_agents)
 
         time_taken = time.time() - time_start
         time_taken_by_controller.append(time_taken)
 
-        # Perform the chosen action on the environment.
-        # The action gets applied to both the local and the remote copy 
-        # of the environment instance, and the observation is what is 
-        # returned by the local copy of the env, and the rewards, and done and info
-        # are returned by the remote copy of the env
         time_start = time.time()
         observation, all_rewards, done, info = remote_client.env_step(action)
         steps += 1
@@ -179,10 +54,6 @@ while True:
 
         if done['__all__']:
             print("Reward : ", sum(list(all_rewards.values())))
-            #
-            # When done['__all__'] == True, then the evaluation of this 
-            # particular Env instantiation is complete, and we can break out 
-            # of this loop, and move onto the next Env evaluation
             break
 
     np_time_taken_by_controller = np.array(time_taken_by_controller)
@@ -199,11 +70,4 @@ while True:
     print("=" * 100)
 
 print("Evaluation of all environments complete...")
-########################################################################
-# Submit your Results
-# 
-# Please do not forget to include this call, as this triggers the 
-# final computation of the score statistics, video generation, etc
-# and is necesaary to have your submission marked as successfully evaluated
-########################################################################
 print(remote_client.submit())
