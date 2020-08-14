9251421b4dfa68cfb36277106a2293a89ca4fcf5
Pascal <pascal.wullschleger@hslu.ch>
2020-08-14 11:49:29 +0200

    Adding setup py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='flatlander',
+   version='0.1',
+   description='Contains flatland challenge solutions',
+   author='Pascal Wullschleger',
+   author_email='pascal.wullschleger@hslu.ch',
+   packages=['flatlander'],
+)
