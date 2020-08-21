f16c35800ea3d6826b004917c7651bd3109be395
Pascal <pascal.wullschleger@hslu.ch>
2020-08-21 13:34:28 +0200

    Only buil in CI

--- flatlander/envs/flatland_sparse_static.py
+++ flatlander/envs/flatland_sparse_static.py
@@ -4,12 +4,35 @@ from numpy.random.mtrand import RandomState
 from flatlander.envs.flatland_sparse import FlatlandSparse
 
 
+def rail_from_grid_transition_map_with_hints(rail_map, hints):
+    """
+    Utility to convert a rail given by a GridTransitionMap map with the correct
+    16-bit transitions specifications.
+
+    Parameters
+    ----------
+    rail_map : GridTransitionMap object
+        GridTransitionMap object to return when the generator is called.
+
+    Returns
+    -------
+    function
+        Generator function that always returns the given `rail_map` object.
+    """
+
+    def generator(width: int, height: int, num_agents: int, num_resets: int = 0,
+                  np_random: RandomState = None):
+        return rail_map, hints
+
+    return generator
+
+
 class FlatlandSparseStatic(FlatlandSparse):
 
     def get_rail_generator(self):
         rail_generator = super().get_rail_generator()
-        rail = rail_generator(25,
-                              25,
-                              5, 0,
-                              RandomState(self._config['seed']))[0]
-        return rail_from_grid_transition_map(rail)
+        rail, hints = rail_generator(25,
+                                     25,
+                                     5, 0,
+                                     RandomState(self._config['seed']))
+        return rail_from_grid_transition_map_with_hints(rail, hints)
