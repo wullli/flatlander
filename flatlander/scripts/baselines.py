45c440a61db7e25bf2565470e81e9b9a77694de1
Pascal <pascal.wullschleger@hslu.ch>
2020-08-21 14:34:15 +0200

    Print all experiments to run

--- flatlander/scripts/baselines.py
+++ flatlander/scripts/baselines.py
@@ -11,8 +11,10 @@ if __name__ == "__main__":
     parser: ArgumentParser = create_parser()
     args = parser.parse_args()
     runner = ExperimentRunner()
-    for baseline_yaml in glob.glob(os.path.join(os.path.dirname(__file__), "..", "resources",
-                                                "baselines", "**/*.yaml")):
+    experiments_files = glob.glob(os.path.join(os.path.dirname(__file__), "..", "resources",
+                                                "baselines", "**/*.yaml"))
+    print("RUNNING BASELINE EXPERIMENTS: ", "\n".join(experiments_files))
+    for baseline_yaml in experiments_files:
         args.config_file = baseline_yaml
         exps = runner.get_experiments(args)
         runner.run(experiments=exps, args=args)
