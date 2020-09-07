a5d5c5a90bdd02cb4da16c7b2655a9d7aebb519f
Pascal <pascal.wullschleger@hslu.ch>
2020-09-07 16:37:16 +0200

    Fix build.sh

--- flatlander/scripts/build.sh
+++ flatlander/scripts/build.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR/../../
 
-docker build -t fl:latest -f ./Dockerfile.gpu --build-arg NB_USER=$USER --build-arg NB_UID=$UID
+docker build -t fl:latest -f ./Dockerfile.gpu --build-arg NB_USER=$USER --build-arg NB_UID=$UID .
