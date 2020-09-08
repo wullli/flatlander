882cee75224c1f5a8618c3db6329f43e5b911cbc
Pascal <pascal.wullschleger@hslu.ch>
2020-09-09 00:28:12 +0200

    Making residual connections optional in transformer

--- flatlander/models/common/transformer.py
+++ flatlander/models/common/transformer.py
@@ -121,14 +121,19 @@ class EncoderLayer(tf.keras.layers.Layer):
     def call(self, x, training, mask):
         attn_output, _ = self.mha(x, x, x, mask)  # (batch_size, input_seq_len, d_model)
         attn_output = self.dropout_1(attn_output, training=training)
-        out1 = self.layer_norm_1(x + attn_output)  # (batch_size, input_seq_len, d_model)
+        if self.use_residual:
+            out1 = self.layer_norm_1(x + attn_output)  # (batch_size, input_seq_len, d_model)
+        else:
+            out1 = self.layer_norm_1(attn_output)  # (batch_size, input_seq_len, d_model)
 
         ffn_output = self.ffn(out1)  # (batch_size, input_seq_len, d_model)
         ffn_output = self.dropout_2(ffn_output, training=training)
+
         if self.use_residual:
             out2 = self.layer_norm_2(ffn_output + out1)  # (batch_size, input_seq_len, d_model)
         else:
             out2 = self.layer_norm_2(ffn_output)  # (batch_size, input_seq_len, d_model)
+
         return out2
 
     @staticmethod
