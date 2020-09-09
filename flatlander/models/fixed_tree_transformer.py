2024b95a77892308f7a484e0b450f822cb8aa776
Pascal <pascal.wullschleger@hslu.ch>
2020-09-09 18:58:11 +0200

    Separate TTFPolicy to file

--- flatlander/models/fixed_tree_transformer.py
+++ flatlander/models/fixed_tree_transformer.py
@@ -2,13 +2,8 @@ import logging
 
 import gym
 import numpy as np
-from ray.rllib import TFPolicy, Policy
-from ray.rllib.agents.ppo import PPOTrainer
-from ray.rllib.agents.ppo.ppo_tf_policy import PPOTFPolicy, KLCoeffMixin, ValueNetworkMixin
 from ray.rllib.models.tf.tf_modelv2 import TFModelV2
-from ray.rllib.policy.tf_policy import EntropyCoeffSchedule
-from ray.rllib.utils import override, DeveloperAPI, try_import_tf
-from ray.tune import register_trainable
+from ray.rllib.utils import try_import_tf
 
 from flatlander.models.common.tree_transformer import Transformer
 
@@ -92,56 +87,3 @@ class FixedTreeTransformer(TFModelV2):
 
     def __del__(self):
         self._traceback()
-
-
-@DeveloperAPI
-class TransformerLearningRateSchedule:
-    """Mixin for TFPolicy that adds a learning rate schedule."""
-
-    @DeveloperAPI
-    def __init__(self, d_model, warmup_steps):
-        self.cur_lr = tf.get_variable("lr", initializer=0.001, trainable=False)
-        self.cur_step = tf.get_variable("cur_step", initializer=1.0, trainable=False)
-        self.d_model = d_model
-        self.d_model = tf.cast(self.d_model, tf.float32)
-        self.warmup_steps = warmup_steps
-
-    @override(Policy)
-    def on_global_var_update(self, global_vars):
-        super(TransformerLearningRateSchedule, self).on_global_var_update(global_vars)
-        self.cur_step.load(float(global_vars["timestep"]), session=self._sess)
-        arg1 = tf.math.rsqrt(self.cur_step)
-        arg2 = self.cur_step * (self.warmup_steps ** -1.5)
-        lr = tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
-        self.cur_lr = lr
-
-    @override(TFPolicy)
-    def optimizer(self):
-        return tf.train.AdamOptimizer(self.cur_lr, beta1=0.9, beta2=0.98, epsilon=1e-9)
-
-
-def setup_mixins(policy, obs_space, action_space, config):
-    ValueNetworkMixin.__init__(policy, obs_space, action_space, config)
-    KLCoeffMixin.__init__(policy, config)
-    EntropyCoeffSchedule.__init__(policy, config["entropy_coeff"],
-                                  config["entropy_coeff_schedule"])
-    warmup_steps = config["model"]["custom_options"].get("warmup_steps", 10000)
-    TransformerLearningRateSchedule.__init__(policy,
-                                             config["model"]["custom_options"]["n_features_per_node"],
-                                             warmup_steps)
-
-
-TTFPPOPolicy = PPOTFPolicy.with_updates(
-    name="TTFPPOPolicy",
-    before_loss_init=setup_mixins,
-    mixins=[
-        TransformerLearningRateSchedule, EntropyCoeffSchedule, KLCoeffMixin,
-        ValueNetworkMixin
-    ])
-
-register_trainable(
-    "TTFPPO",
-    PPOTrainer.with_updates(
-        name="CCPPOTrainer", get_policy_class=lambda c: TTFPPOPolicy
-    ),
-)
