760af266c5f771902c427c1592d86e58546c34c4
Pascal <pascal.wullschleger@hslu.ch>
2020-10-16 14:50:26 +0200

    Fix learning rate warmup

--- flatlander/trainers/dqn.py
+++ flatlander/trainers/dqn.py
@@ -1,8 +1,10 @@
+import gym
 from ray.rllib import Policy, TFPolicy
 from ray.rllib.agents.dqn.apex import ApexTrainer
 from ray.rllib.agents.dqn.dqn_tf_policy import DQNTFPolicy, ComputeTDErrorMixin
 from ray.rllib.agents.dqn.simple_q_tf_policy import TargetNetworkMixin
 from ray.rllib.utils import try_import_tf, override, DeveloperAPI
+from ray.rllib.utils.typing import TrainerConfigDict
 from ray.tune import register_trainable
 
 tf1, tf, tfv = try_import_tf()
@@ -34,17 +36,28 @@ class TransformerLearningRateSchedule:
         return tf1.train.AdamOptimizer(self.cur_lr, beta1=0.9, beta2=0.98, epsilon=1e-9)
 
 
-def setup_mid_mixins(policy: Policy, _1, _2, config) -> None:
-    ComputeTDErrorMixin.__init__(policy)
+def setup_early_mixins(policy: Policy, _1, _2, config) -> None:
     warmup_steps = config["model"]["custom_model_config"].get("warmup_steps", 100000)
     TransformerLearningRateSchedule.__init__(policy,
                                              config["model"]["custom_model_config"]["transformer"]["num_heads"],
                                              warmup_steps)
 
 
+def setup_mid_mixins(policy: Policy, _1, _2, config) -> None:
+    ComputeTDErrorMixin.__init__(policy)
+
+
+def setup_late_mixins(policy: Policy, obs_space: gym.Space,
+                      action_space: gym.Space,
+                      config: TrainerConfigDict) -> None:
+    TargetNetworkMixin.__init__(policy, obs_space, action_space, config)
+
+
 WarmupDQNTFPolicy = DQNTFPolicy.with_updates(
     name="WarmupDQNTFPolicy",
+    before_init=setup_early_mixins,
     before_loss_init=setup_mid_mixins,
+    after_init=setup_late_mixins,
     mixins=[
         TargetNetworkMixin,
         ComputeTDErrorMixin,
