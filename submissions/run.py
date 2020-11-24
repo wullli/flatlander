b73c3560ca89ca76f87e2ca3a5b9303512fa4a09
Pascal Wullschleger <pascal.wullschleger@hslu.ch>
2020-11-24 11:26:51 +0100

    Eval shortest path agent on flatland eval

--- submissions/run.py
+++ submissions/run.py
@@ -37,9 +37,7 @@ def evaluate(config, run):
     evaluation_number = 0
     total_reward = 0
     all_rewards = []
-    prio_agent = get_agent(config, run)
     sp_agent = ShortestPathAgent()
-    num_explorations = 3
 
     while True:
         try:
@@ -52,53 +50,13 @@ def evaluate(config, run):
 
             evaluation_number += 1
             episode_start_info(evaluation_number, remote_client=remote_client)
-            robust_env = RobustFlatlandGymEnv(rail_env=remote_client.env,
-                                              max_nr_active_agents=100,
-                                              observation_space=None,
-                                              priorizer=NrAgentsSameStart(),
-                                              allow_noop=True)
-
-            priorities = prio_agent.compute_actions(observation, explore=False)
-            sorted_actions = {k: v for k, v in
-                              sorted(priorities.items(), key=lambda item: item[1], reverse=True)}
-            sorted_handles = list(sorted_actions.keys())
-
-            priorizations = []
-
-            if remote_client.env.get_num_agents() <= 5 and EXPLORE:
-                for i in range(num_explorations):
-                    obs = observation
-                    if i != 0:
-                        priorities = prio_agent.compute_actions(obs, explore=True)
-                        sorted_actions = {k: v for k, v in
-                                          sorted(priorities.items(), key=lambda item: item[1], reverse=True)}
-                        sorted_handles = list(sorted_actions.keys())
-                    env = deepcopy(remote_client.env)
-                    ep_return = 0
-                    done = defaultdict(lambda: False)
-
-                    print("Explore priorizations step", i + 1)
-
-                    while not done['__all__']:
-                        actions = ShortestPathAgent().compute_actions(obs, env)
-                        robust_actions = robust_env.get_robust_actions(actions, sorted_handles)
-                        obs, all_rewards, done, info = env.step(robust_actions)
-                        steps += 1
-                        ep_return += np.sum(list(all_rewards.values()))
-
-                    priorizations.append((ep_return, sorted_handles))
-
-            if len(priorizations) > 0:
-                sorted_handles = max(priorizations, key=lambda t: t[0])[1]
 
             done = defaultdict(lambda: False)
             while True:
                 try:
                     while not done['__all__']:
                         rail_actions = sp_agent.compute_actions(observation, remote_client.env)
-                        robust_actions = robust_env.get_robust_actions(rail_actions, sorted_handles=sorted_handles)
-
-                        observation, all_rewards, done, info = remote_client.env_step(robust_actions)
+                        observation, all_rewards, done, info = remote_client.env_step(rail_actions)
                         steps += 1
                         print('.', end='', flush=True)
 
